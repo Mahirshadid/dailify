19841e1188108f1074016be6a4f50506ba18a2b0
Mahir Shadid <mahir.shadid@gmail.com>
2023-10-27 21:50:16 +0600

    day-4

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
+import './footer.css'
+
+import logo from '../../public/text-logo.png'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className='footer_contents'>
+        <a href='#'><Image src={logo} alt='' 
+        style={{width:'15%',height:'15%',minWidth:'100px'}}/></a>
+        <p><a href=''>Developer's Thoughts</a></p>
+        <p>All Rights Reserved @ Mahir Shadid</p>
+      </div>
+    </div>
   )
 }
 
